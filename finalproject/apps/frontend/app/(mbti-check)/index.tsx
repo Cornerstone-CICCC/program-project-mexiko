594151a56cd6c9e00516abee44d14d6e27392293
dueon <hdejh79@gmail.com>
2026-03-13 19:44:00 -0700

    mobile frontend initial setup

---
+++ finalproject/apps/frontend/app/(mbti-check)/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MBTIHome = () => {
+  return (
+    <View>
+      <Text>MBTIHome</Text>
+    </View>
+  );
+};
+
+export default MBTIHome;
+
+const styles = StyleSheet.create({});
---
+++ finalproject/apps/frontend/app/(mbti-check)/questions.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const questions = () => {
+  return (
+    <View>
+      <Text>questions</Text>
+    </View>
+  );
+};
+
+export default questions;
+
+const styles = StyleSheet.create({});
