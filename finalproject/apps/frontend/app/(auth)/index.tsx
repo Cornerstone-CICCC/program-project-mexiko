efe5223f0be3178fb926cec03e3505a4238eec66
dueon <hdejh79@gmail.com>
2026-03-18 09:59:37 -0700

    feat:MBTI test frontend implementation

--- finalproject/apps/frontend/app/(auth)/index.tsx
+++ finalproject/apps/frontend/app/(auth)/index.tsx
@@ -1,11 +1,17 @@
 import { Text, View } from "react-native";
- 
+import { Link } from "expo-router";
+import { Button } from "@/components/Button";
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
+
+      <Link href="/(mbti-check)" push asChild>
+        <Button title="mbti TEst" />
+      </Link>
     </View>
   );
 }
